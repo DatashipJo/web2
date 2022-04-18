94bae1d5ba2c8e49ddd49f9b52366299246dda3f
hab1nSong <thdgkqls0507@naver.com>
2022-04-18 12:29:33 +0900

    test

---
+++ DataShipJo/src/main/java/app/review/dstp/DataShipJoApplication.java
@@ -0,0 +1,13 @@
+package app.review.dstp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataShipJoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataShipJoApplication.class, args);
+	}
+
+}
---
+++ DataShipJo/src/test/java/app/review/dstp/DataShipJoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.review.dstp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataShipJoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
