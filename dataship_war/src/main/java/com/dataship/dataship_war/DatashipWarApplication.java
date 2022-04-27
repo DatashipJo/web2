9c2da242835f2764bfaaab22e5cf2bdbaba8a845
Hydra✧ <96683379+coderhydra@users.noreply.github.com>
2022-04-27 23:44:48 +0900

    main page  보드 링크 생성 LK

---
+++ dataship_war/src/main/java/com/dataship/dataship_war/DatashipWarApplication.java
@@ -0,0 +1,13 @@
+package com.dataship.dataship_war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatashipWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatashipWarApplication.class, args);
+    }
+
+}
