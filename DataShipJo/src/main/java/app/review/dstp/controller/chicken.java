b69e27da790c96b584279e8706ef28bc1ad5592b
hab1nSong <thdgkqls0507@naver.com>
2022-05-13 19:20:28 +0900

    list to detail

--- DataShipJo/src/main/java/app/review/dstp/controller/chicken.java
+++ DataShipJo/src/main/java/app/review/dstp/controller/chicken.java
@@ -1,8 +1,6 @@
 package app.review.dstp.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,12 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.google.gson.Gson;
 
 import app.review.dstp.list.StoreDao;
 import app.review.dstp.service.StoreService;
@@ -45,7 +39,7 @@ public class chicken
 	@GetMapping("/board/{area}")
     public String board(@PathVariable("area") String area, Model m){
         m.addAttribute("tableList", svc.getChickenList2(area));
-        return "thymeleaf/list_copy";
+        return "thymeleaf/chicken_list";
     }
 	
     @GetMapping("detail/{num}")
@@ -53,24 +47,12 @@ public class chicken
 //    	m.addAttribute("tableList", dao.getDetailList2(num));
     	m.addAttribute("store", dao.getStore(num));
     	m.addAttribute("num", num);
-        return "thymeleaf/list_copy4";
-    }
-
-    @GetMapping("/test")
-    public String test(Model m) {
-    	m.addAttribute("tableList", dao.getChickenList2());
-    	return "thymeleaf/list_copy2";
+        return "thymeleaf/chicken_detail";
     }
     
     @PostMapping(value="/selectAll")
     @ResponseBody
     public List<StoreVo> MoreView(StoreVo paramVo) {
     	return dao.getDetailList(paramVo);
-    }
-    
-    @GetMapping("/page2")
-    public String page2(Model m) {
-    	m.addAttribute("tableList", dao.getChickenList2());
-    	return "thymeleaf/list_copy3";
     }
 }
