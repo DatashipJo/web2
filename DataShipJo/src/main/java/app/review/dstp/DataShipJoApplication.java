bbb510c7f94b8f2693ecbf8b06b58ea003ca6e3e
hab1nSong <thdgkqls0507@naver.com>
2022-05-20 21:16:07 +0900

    Chicken Map

--- DataShipJo/src/main/java/app/review/dstp/DataShipJoApplication.java
+++ DataShipJo/src/main/java/app/review/dstp/DataShipJoApplication.java
@@ -2,12 +2,21 @@ package app.review.dstp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DataShipJoApplication {
+public class DataShipJoApplication extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataShipJoApplication.class, args);
 	}
+	
+	@Override 
+   protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) 
+   { 
+      return builder.sources(DataShipJoApplication.class); 
+   }
 
 }
